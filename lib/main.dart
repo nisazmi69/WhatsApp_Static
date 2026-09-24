b645d129d4b4cd0453f7f9513b737b7fcf9628ef
n26959330-sys <nisazmi113@gmail.com>
2026-09-24 17:07:16 +0700

    tambah pesan

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
@@ -32,7 +27,7 @@ class _HomePageState extends State<HomePage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          title: Text("WhatsApp"),
+          title: Text('Whatsapp'),
           backgroundColor: Colors.green,
           bottom: TabBar(
             tabs: [
@@ -45,82 +40,183 @@ class _HomePageState extends State<HomePage> {
 
         body: TabBarView(
           children: [
-
-            // TAB CHAT
-            Column(
+            ListView(
               children: [
                 Card(
                   elevation: 5,
                   child: ListTile(
-                    leading: Icon(
-                      Icons.person,
-                    ),
-                    title: Text("Ari"),
-                    subtitle: Text("OMAIGATT"),
-                    trailing: Text(
-                      "50",
-                      style: TextStyle(
-                        color: Colors.green,
-                      ),
-                    ),
+                    leading: Icon(Icons.person),
+                    title: Text("bb"),
+                    subtitle: Text('iyaa beyy'),
+                    trailing: Text('999+', style: TextStyle(color: Colors.green)),
                   ),
                 ),
 
                 Card(
                   elevation: 5,
                   child: ListTile(
-                    leading: Icon(
-                      Icons.person,
-                    ),
-                    title: Text("Yuni"),
-                    subtitle: Text("OKEE"),
-                    trailing: Text(
-                      "50",
-                      style: TextStyle(
-                        color: Colors.green,
-                      ),
-                    ),
+                    leading: Icon(Icons.person),
+                    title: Text("lele"),
+                    subtitle: Text('xiu'),
+                    trailing: Text('5', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("x"),
+                    subtitle: Text('hai bb'),
+                    trailing: Text('49', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("manu"),
+                    subtitle: Text('woi'),
+                    trailing: Text('26', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("bocil"),
+                    subtitle: Text('Beli air'),
+                    trailing: Text('30', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("yoyo"),
+                    subtitle: Text('p ayuu'),
+                    trailing: Text('58', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("kimpul"),
+                    subtitle: Text('aku gapunya barangnya'),
+                    trailing: Text('51', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("iput"),
+                    subtitle: Text('wey muka ku kotak'),
+                    trailing: Text('2', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("idk"),
+                    subtitle: Text('ka, mau ga jadi gf aku......'),
+                    trailing: Text('9', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("ica"),
+                    subtitle: Text('well dah'),
+                    trailing: Text('23', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("rara"),
+                    subtitle: Text('makasih ya'),
+                    trailing: Text('67', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Qila"),
+                    subtitle: Text('Beli jco yuu'),
+                    trailing: Text('5', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Caca"),
+                    subtitle: Text('main yu saa'),
+                    trailing: Text('999+', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("saa"),
+                    subtitle: Text('bagus bgt bunganya'),
+                    trailing: Text('100', style: TextStyle(color: Colors.green)),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("asmiz"),
+                    subtitle: Text('caffe yuk'),
+                    trailing: Text('18', style: TextStyle(color: Colors.green)),
                   ),
                 ),
               ],
             ),
 
-            // TAB STATUS
-            Card(
-              elevation: 5,
-              child: ListTile(
-                leading: Icon(
-                  Icons.person,
+            Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.add_circle_outline_sharp),
+                  title: Text('My status'),
+                  subtitle: Text('Tap to add status update'),
                 ),
-                title: Text("Status saya"),
-                subtitle: Text(
-                  "ketuk untuk menambahkan status",
+                Text('Recent updates'),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('lele'),
+                  subtitle: Text('2 minutes ago'),
                 ),
-                trailing: Icon(
-                  Icons.add_circle,
-                  color: Colors.green,
-                ),
-              ),
+              ],
             ),
 
-            // TAB PANGGILAN
-            Card(
-              elevation: 5,
-              child: ListTile(
-                leading: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.person),
-                    Icon(Icons.call_received),
-                  ],
+            Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.call),
+                  title: Text('lele'),
+                  subtitle: Text(
+                    '15 minutes ago',
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ),
-                title: Text("Ari"),
-                subtitle: Text("23.00"),
-                trailing: Icon(
-                  Icons.call,
-                  color: Colors.green,
-                ),
-              ),
+              ],
             ),
           ],
         ),
